1a1d62916de9e53a111a48b5edcd9bc1daf23a61
David Bernheisel <david@bernheisel.com>
2019-09-10 10:02:12 -0400

    Update Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
-import wishcastrImg from '../assets/wishcastr-sample.png'
-import numberGuessingImg from '../assets/numberguessing-sample.png'
-import blackjackImg from '../assets/blackjack-sample.png'
-import battleshipImg from '../assets/battleship-sample.png'
-import weatherReportImg from '../assets/weatherreport-sample.png'
 
 import Layout from '../components/layout'
 
@@ -27,41 +22,17 @@ class ProjectsPage extends React.Component {
         />
 
         <article>
-          <h2>Wishcastr</h2>
-          <OutboundLink href="https://github.com/wishcastr/wishcastr">
-            <img src={wishcastrImg} alt="Wishcastr" />
+          <h2>DateTimeParser</h2>
+          <OutboundLink href="https://github.com/taxjar/date_time_parser">
             GitHub
           </OutboundLink>
-          <p className="italic">curently does not work since Amazon API key expired</p>
           <p>Major functions:</p>
           <ul>
-            <li>Users can set price thresholds on products and get notified when those products hit the threshold.</li>
-            <li>Built on Rails 5 (API backend) and AngularJS 1.4 (Frontend)</li>
-            <li>OAuth with Amazon Login</li>
-            <li>Completely custom; no additional Ruby gems, CSS frameworks, or third-party Angular libaries.</li>
-            <li>Background cron job to manage price updates</li>
-            <li>Email notifications through Mailgun</li>
-            <li>Consumes and normalizes Amazon Product Advertising API.</li>
+            <li>Parse an Elixir DateTime, NaiveDateTime, Date, or Time from a string</li>
+            <li>Tokenizes found parts of the datetime through binary matching</li>
+            <li>Suppors major ISO formats</li>
+            <li>Supports arbitrary formats, eg "Saturday, Jan-34-04"</li>
           </ul>
-        </article>
-
-
-        <article>
-          <h2>Command-line Utilities (for fun)</h2>
-          <div className="flex flex-wrap">
-            <OutboundLink className="w-1/2 p-2" href="https://github.com/dbernheisel/weather_report">
-              <img src={weatherReportImg} alt="WeatherReport" />
-            </OutboundLink>
-            <OutboundLink className="w-1/2 p-2" href="https://github.com/dbernheisel/battleship">
-              <img src={battleshipImg} alt="Battleship" />
-            </OutboundLink>
-            <OutboundLink className="w-1/2 p-2" href="https://github.com/dbernheisel/blackjack_advisor">
-              <img src={blackjackImg} alt="Blackjack" />
-            </OutboundLink>
-            <OutboundLink className="w-1/2 p-2" href="https://github.com/dbernheisel/guessing_game">
-              <img src={numberGuessingImg} alt="Number Guessing Game" />
-            </OutboundLink>
-          </div>
         </article>
       </Layout>
     )
