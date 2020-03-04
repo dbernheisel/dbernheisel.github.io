0078474a8540bc89e78ced7c6245cbc415fa280a
David Bernheisel <david@bernheisel.com>
2020-03-03 23:19:33 -0500

    New ElixirLS and Vim post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,8 +13,8 @@ const BlogFooter = props => {
     <div className="flex flex-wrap justify-between">
       {previous && (
         <div className="w-full p-2 lg:w-1/2">
-          <Link className="flex-1 button bg-purple-darker inline-flex items-center" to={previous.fields.slug} rel="prev">
-            <svg className="fill-current w-6 h-6 mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1408 960v-128q0-26-19-45t-45-19h-502l189-189q19-19 19-45t-19-45l-91-91q-18-18-45-18t-45 18l-362 362-91 91q-18 18-18 45t18 45l91 91 362 362q18 18 45 18t45-18l91-91q18-18 18-45t-18-45l-189-189h502q26 0 45-19t19-45zm256-64q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
+          <Link className="inline-flex items-center flex-1 button bg-purple-darker" to={previous.fields.slug} rel="prev">
+            <svg className="w-6 h-6 mr-2 fill-current" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1408 960v-128q0-26-19-45t-45-19h-502l189-189q19-19 19-45t-19-45l-91-91q-18-18-45-18t-45 18l-362 362-91 91q-18 18-18 45t18 45l91 91 362 362q18 18 45 18t45-18l91-91q18-18 18-45t-18-45l-189-189h502q26 0 45-19t19-45zm256-64q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
             <span>{previous.frontmatter.title}</span>
           </Link>
         </div>
@@ -22,9 +22,9 @@ const BlogFooter = props => {
 
       {next && (
         <div className="w-full p-2 lg:w-1/2">
-          <Link className="flex-1 button bg-purple-darker inline-flex items-center" to={next.fields.slug} rel="next">
+          <Link className="inline-flex items-center flex-1 button bg-purple-darker" to={next.fields.slug} rel="next">
             <span>{next.frontmatter.title}</span>
-            <svg className="fill-current w-6 h-6 ml-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1413 896q0-27-18-45l-91-91-362-362q-18-18-45-18t-45 18l-91 91q-18 18-18 45t18 45l189 189h-502q-26 0-45 19t-19 45v128q0 26 19 45t45 19h502l-189 189q-19 19-19 45t19 45l91 91q18 18 45 18t45-18l362-362 91-91q18-18 18-45zm251 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
+            <svg className="w-6 h-6 ml-2 fill-current" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1413 896q0-27-18-45l-91-91-362-362q-18-18-45-18t-45 18l-91 91q-18 18-18 45t18 45l189 189h-502q-26 0-45 19t-19 45v128q0 26 19 45t45 19h502l-189 189q-19 19-19 45t19 45l91 91q18 18 45 18t45-18l362-362 91-91q18-18 18-45zm251 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
           </Link>
         </div>
       )}
@@ -47,9 +47,9 @@ class BlogPostTemplate extends React.Component {
           title={`${post.frontmatter.title} | ${siteTitle}`}
         />
 
-        <article class="blog-post">
-          <h1 className="text-3xl mb-0">{post.frontmatter.title}</h1>
-          <p className="frontmatter mt-0 mb-8 text-sm">
+        <article className="blog-post">
+          <h1 className="mb-0 text-3xl">{post.frontmatter.title}</h1>
+          <p className="mt-0 mb-8 text-sm frontmatter">
             <span>
               Published on {post.frontmatter.date}
             </span>
