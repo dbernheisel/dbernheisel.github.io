ca7da3f2daa4debfdb6c096705334f3894314773
David Bernheisel <david@bernheisel.com>
2020-07-24 15:54:31 -0400

    Add Elixir Utilities project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,7 +21,7 @@ class ProjectsPage extends React.Component {
           title={siteTitle}
         />
         <div className="flex flex-col space-y-4">
-          <article className="">
+          <article>
             <h2 className="tracking-wider text-2xl font-bold">DateTimeParser</h2>
             <div className="space-x-4">
               <OutboundLink className="link" href="https://github.com/taxjar/date_time_parser">
@@ -37,6 +37,25 @@ class ProjectsPage extends React.Component {
               <li className="my-2">Tokenizes found parts of the datetime through binary matching</li>
               <li className="my-2">Supports major ISO formats</li>
               <li className="my-2">Supports arbitrary formats, eg "Saturday, Jan-34-04"</li>
+            </ul>
+          </article>
+
+          <article>
+            <h2 className="tracking-wider text-2xl font-bold">Elixir Utilities</h2>
+            <div className="space-x-4">
+              <OutboundLink className="link" href="https://github.com/dbernheisel/elixir-utilities-web">
+                GitHub
+              </OutboundLink>
+              <OutboundLink className="link" href="https://elixir-utilities.herokuapp.com">
+                Visit
+              </OutboundLink>
+            </div>
+            <p>Major functions:</p>
+            <ul className="pl-4 list-disc">
+              <li className="my-2">LiveView-powered Regex visualizer.</li>
+              <li className="my-2">See the raw result and the visualized result</li>
+              <li className="my-2">You can permalink regexes to share with others</li>
+              <li className="my-2">Cheatsheet and recipes</li>
             </ul>
           </article>
 
