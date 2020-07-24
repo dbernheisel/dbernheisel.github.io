2d20a04f5b202d147012fbf2c596eee466ce4e93
David Bernheisel <david@bernheisel.com>
2020-07-24 15:55:00 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-be644e17620554eca688.js"
+    "url": "webpack-runtime-5be88de751054daa4121.js"
   },
   {
     "url": "framework-1cbcc4f69858806ac5db.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f0fd629e6eec00b9d8baaeac266f2943"
+    "revision": "df03b23e2c9547e69d346cf110685b40"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8dc5b1de7df84c955e11.js"
